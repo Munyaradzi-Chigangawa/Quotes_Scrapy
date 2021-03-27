ddda5a1b82db2f53855d7d600edcca1da632c3a0
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2021-03-27 12:51:01 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+import 'ui/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(
+        backgroundColor: Colors.black,
+        title: Text('Quotes Scrapy',
+        style: TextStyle(
+          color: Colors.white),
+        ),
+      navigateAfterSeconds: Home(),
+      )
+    );
+  }
+}
